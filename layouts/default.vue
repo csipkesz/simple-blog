03606a27f10639765181730f4828c276635a27bb
csipkesz <diesellucio@gmail.com>
2023-07-31 16:16:11 +0200

    [MOD] responsive navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,45 @@
 <template>
     <header class="shadow-md bg-white">
-        <nav class="container mx-auto p-6 flex justify-between">
-            <NuxtLink to="/">HOME</NuxtLink>
-            <ul class="flex gap-7">
-                <li><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
-                <li><NuxtLink to="/post/">Blog</NuxtLink></li>
-                <li><NuxtLink to="/post/3">Menüpont</NuxtLink></li>
-                <li><NuxtLink to="/post/4">Menüpont</NuxtLink></li>
-            </ul>
-
+        <nav class="flex flex-wrap items-center justify-between py-4 md:py-0 px-16 text-lg text-gray-700 bg-white">
+            <div>
+                <NuxtLink to="/"><img class="max-w-sm content-center p-3" src="~/assets/images/p_logo.png"></NuxtLink> 
+            </div>
+        
+            <button id="menu-button" class="text-3xl cursor-pointer md:hidden block">
+                <nuxt-icon name="menu"/>
+            </button>
+        
+            <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
+                <ul
+                    class="
+                    pt-4
+                    text-base text-gray-700
+                    md:flex
+                    md:justify-between 
+                    md:pt-0"
+                >
+                    <li class="md:p-4 py-2 block text-xl hover:font-bold"><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
+                    <li class="md:p-4 py-2 block text-xl hover:font-bold"><NuxtLink to="/post/">Blog</NuxtLink></li>
+                    <li class="md:p-4 py-2 block text-xl hover:font-bold"><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
+                    <li class="md:p-4 py-2 block text-xl hover:font-bold"><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
+                    
+                </ul>
+            </div>
         </nav>
     </header>
-    <div class="container mx-auto p-4">
+    <div class="container px-16 p-4">
         <slot />
     </div>
 </template>
+
+<script setup>
+onMounted(() => {
+    const button = document.querySelector('#menu-button');
+    const menu = document.querySelector('#menu');
+    
+    
+    button.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
+})
+</script>
