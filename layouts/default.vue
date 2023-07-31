ecb62ca04f33c18805e6f6ca4afac5bedd9e53c1
csipkesz <diesellucio@gmail.com>
2023-07-31 14:53:55 +0200

    [NEW] Articles multiple row design

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
             <NuxtLink to="/">HOME</NuxtLink>
             <ul class="flex gap-7">
                 <li><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
-                <li><NuxtLink to="/post/1">Blog</NuxtLink></li>
-                <li><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
-                <li><NuxtLink to="/post/1">Menüpont</NuxtLink></li>
+                <li><NuxtLink to="/post/">Blog</NuxtLink></li>
+                <li><NuxtLink to="/post/3">Menüpont</NuxtLink></li>
+                <li><NuxtLink to="/post/4">Menüpont</NuxtLink></li>
             </ul>
 
         </nav>
---
+++ pages/post/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="row pt-16 pb-8 pl-4 font-bold text-5xl">Legfrisebb cikkeink</div>
+
+
+        <div class="container flex flex-wrap">
+            <div class="h-full p-4 lg:w-1/4">
+                <div class="max-w-sm">
+                    <img class="rounded-xl" src="https://wallpaperaccess.com/full/138728.jpg" alt="" />
+                    <div class="pt-2 pb-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">This is the hír, Johny is Here</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full p-4 lg:w-1/4">
+                <div class="max-w-sm">
+                    <img class="rounded-xl" src="https://wallpaperaccess.com/full/138728.jpg" alt="" />
+                    <div class="pt-2 pb-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">This is the hír, Johny is Here</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full p-4 lg:w-1/4">
+                <div class="max-w-sm">
+                    <img class="rounded-xl" src="https://wallpaperaccess.com/full/138728.jpg" alt="" />
+                    <div class="pt-2 pb-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">This is the hír, Johny is Here</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full p-4 lg:w-1/4">
+                <div class="max-w-sm">
+                    <img class="rounded-xl" src="https://wallpaperaccess.com/full/138728.jpg" alt="" />
+                    <div class="pt-2 pb-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">This is the hír, Johny is Here</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full p-4 lg:w-1/4">
+                <div class="max-w-sm">
+                    <img class="rounded-xl" src="https://wallpaperaccess.com/full/138728.jpg" alt="" />
+                    <div class="pt-2 pb-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">This is the hír, Johny is Here</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full p-4 lg:w-1/4">
+                <div class="max-w-sm">
+                    <img class="rounded-xl" src="https://wallpaperaccess.com/full/138728.jpg" alt="" />
+                    <div class="pt-2 pb-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">This is the hír, Johny is Here</h5>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
