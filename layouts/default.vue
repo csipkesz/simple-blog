549ba3bc4b3fdef005460df1b6ad03f4e9f70a55
csipkesz <diesellucio@gmail.com>
2023-07-31 13:26:58 +0200

    [NEW] Adding layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="/">HOME</NuxtLink>
+            <ul>
+                <li><NuxtLink to="/post/1">Post</NuxtLink></li>
+            </ul>
+
+        </nav>
+    </header>
+    <div>
+        <slot />
+    </div>
+</template>
