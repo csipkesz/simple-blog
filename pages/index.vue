5c000e6a4d36fdbba3e0e728fbfd9fc6558bae2b
csipkesz <diesellucio@gmail.com>
2023-07-31 13:14:55 +0200

    [INIT] Project INIT

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero minima molestiae voluptatibus hic necessitatibus, beatae ex, debitis exercitationem, iusto placeat nostrum! Harum eius nisi, deserunt omnis ducimus ex. Placeat, asperiores?</p>
+    </div>
+</template>
+
+<script setup>
+
+onMounted(() => {
+
+});
+
+</script>
+
+<style scoped>
+
+</style>
