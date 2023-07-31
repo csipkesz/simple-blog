afe9f89035b90dbe48d6d1af9a6d6cd823790ed7
csipkesz <diesellucio@gmail.com>
2023-07-31 13:20:21 +0200

    [MOD] Post page with id param

---
+++ pages/post/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>BLOG POST FOR {{ postId }}</h2>
+    </div>
+</template>
+
+<script setup>
+    const { id: postId } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
